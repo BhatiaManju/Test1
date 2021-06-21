57fdb969f35ae2f983da944abd1f23be45eabaf4
BManju <60623872+BhatiaManju@users.noreply.github.com>
2021-06-21 05:42:01 +0000

    added new code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('Hello World!')
+num1 = int(input("Enter a number: ")) 
+num2 = int(input("Enter another number: ")) 
+equal = False 
+if num1 > num2: 
+	greater = num1 
+elif num2 > num1: 
+	greater = num2 
+elif num1 == num2: 
+	equal = True 
+if equal: 
+	print("The numbers are equal.") 
+else: 
+	print("The greatest number is " + str(greater))
